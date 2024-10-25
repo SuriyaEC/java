2382b1877ea839258df9fba47256e1fa54564f85
Admin <Admin@DESKTOP-4S1TDQE>
2024-10-25 21:16:11 +0530

    java basic

---
+++ src/pack1/ADD.java
@@ -0,0 +1,16 @@
+package pack1;
+
+import java.util. *;
+public class ADD {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println(a+"+"+b+"="+c);
+		
+	}
+
+}
